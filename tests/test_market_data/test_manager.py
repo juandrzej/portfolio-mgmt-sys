793027210b32d16fe280d462e6ae023dd647c105
juandrzej <programming.yippee729@passinbox.com>
2025-08-24 15:26:47 +0200

    adjust pyright config to designated file, add new test

--- tests/test_market_data/test_manager.py
+++ tests/test_market_data/test_manager.py
@@ -1,6 +1,21 @@
+import pytest
 from market_data.manager import MarketDataManager
 
 def test_singleton_basic():
+    # reset instance, only need for multiple singleton tests
+    MarketDataManager._instance = None
+
     manager1 = MarketDataManager()
     manager2 = MarketDataManager()
     assert manager1 is manager2
+
+def test_empty_none_fetch():
+    MarketDataManager._instance = None
+
+    manager = MarketDataManager()
+
+    with pytest.raises(ValueError):
+        manager.fetch_data_daily("")
+
+    with pytest.raises(ValueError):
+        manager.fetch_data_daily(None)
