6940651d00ef6a3d94b7eb326f4832ac2e54a569
juandrzej <programming.yippee729@passinbox.com>
2025-08-24 20:13:05 +0200

    add base url test, imporove fetch data test to use parametrize (its fricking amazing rly)

--- tests/test_market_data/test_manager.py
+++ tests/test_market_data/test_manager.py
@@ -1,21 +1,21 @@
 import pytest
 from market_data.manager import MarketDataManager
 
-def test_singleton_basic():
+def test_singleton_logic():
     # reset instance, only need for multiple singleton tests
     MarketDataManager._instance = None
-
     manager1 = MarketDataManager()
     manager2 = MarketDataManager()
     assert manager1 is manager2
 
-def test_empty_none_fetch():
+@pytest.mark.parametrize('param', ['', None])
+def test_empty_none_fetch(param):
     MarketDataManager._instance = None
-
     manager = MarketDataManager()
-
     with pytest.raises(ValueError):
-        manager.fetch_data_daily("")
-
-    with pytest.raises(ValueError):
-        manager.fetch_data_daily(None)
+        manager.fetch_data_daily(param)
+        
+def test_base_url():
+    MarketDataManager._instance = None
+    manager = MarketDataManager()
+    assert manager.base_url == 'https://www.alphavantage.co/query'
