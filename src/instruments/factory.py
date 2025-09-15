448fbeac8e88b0a3602cc41d8050ff964772fc47
juandrzej <programming.yippee729@passinbox.com>
2025-09-15 17:58:19 +0200

    start working on the factory itself

--- src/instruments/factory.py
+++ src/instruments/factory.py
@@ -1,3 +1,31 @@
+from instruments.base import FinancialInstrument, InstrumentType
+from instruments.bond import Bond
+from instruments.equity import Equity
+
+
 class InstrumentFactory:
     """Creates different types of financial instruments"""
-    pass
+
+    @staticmethod
+    def create_instrument(instrument_type: InstrumentType, **kwargs) -> FinancialInstrument:
+        match instrument_type:
+            case InstrumentType.EQUITY_LONG | InstrumentType.EQUITY_SHORT:
+                return Equity(
+                    symbol=kwargs["symbol"],
+                    price=kwargs["price"],
+                    quantity=kwargs["quantity"],
+                    beta=kwargs.get("beta", 1.0),
+                    sector=kwargs.get("sector", ""),
+                    dividend=kwargs.get("dividend", 0),
+                )
+            case InstrumentType.BOND:
+                return Bond(
+                    symbol=kwargs["symbol"],
+                    price=kwargs["price"],
+                    quantity=kwargs["quantity"],
+                    coupon_rate=kwargs["coupon_rate"],
+                    maturity=kwargs["maturity"],
+                    credit_rating=kwargs.get("credit_rating", "AAA")
+                )
+            case _:
+                raise ValueError(f"Unknown instrument type: {instrument_type}")
